deaa418342df85568bd4eccc662cc31e544e9548
vagrant <vagrant@vagrant.vm>
2017-07-06 07:01:21 +0000

    Formatted flights display table

---
+++ static/js/scripts.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+
+//     function submitUserCriteria (evt) {
+//         // add later, remove hardcoded data
+//         // var formInput = {
+//         //     "origin": $("#origin").val(),
+//         //     "budget": $("#budget").val(),
+//         //     "start-date": $("#start-date").val(),
+//         //     "return-date": $("#return-date").val()
+//         // };
+
+//         // remove below hardcoded data later
+//         var formInput = {
+//             "origin": "SFO",
+//             "budget": "500",
+//             "start-date": 08/21/2017,
+//             "return-date": 08/25/2017
+//         };
+
+
+// }
+
+// $("#user-criteria").on("submit", submitUserCriteria);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    });
